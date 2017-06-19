57fb70c00a5beaf0fd503d1193cc33f75b6f9ddc
Fabian Zeiher <fzeiher@gmail.com>
2017-06-19 17:54:32 +0200

    FIX: no entry in name and breed is now displayed in catalog

--- app/src/main/java/com/example/android/pets/PetCursorAdapter.java
+++ app/src/main/java/com/example/android/pets/PetCursorAdapter.java
@@ -2,6 +2,7 @@ package com.example.android.pets;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -62,6 +63,9 @@ public class PetCursorAdapter extends CursorAdapter {
 
         String name = cursor.getString(cursor.getColumnIndexOrThrow(PetEntry.COLUMN_PET_NAME));
         String breed = cursor.getString(cursor.getColumnIndexOrThrow(PetEntry.COLUMN_PET_BREED));
+
+        if (TextUtils.isEmpty(name)) { name = "Unknown Name"; }
+        if (TextUtils.isEmpty(breed)) { breed = "Unknown Breed"; }
 
         nameTextView.setText(name);
         summaryTextView.setText(breed);
